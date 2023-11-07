81c18ab152693ac4ac13c976504d0d3c34dfe0a5
Bhavana Nangare <60853406+akshunangare15@users.noreply.github.com>
2023-11-07 15:48:26 -0800

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  getRepositories(): any[] | undefined {
+    throw new Error('Method not implemented.');
+  }
+  getUserRepos(username: string) {
+    throw new Error('Method not implemented.');
+  }
+  constructor(private httpClient: HttpClient) {}
 
   getUser(githubUsername: string) {
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername: string) {
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos`);
+  }
 }
