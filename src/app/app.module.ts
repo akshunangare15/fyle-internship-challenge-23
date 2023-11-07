3a7e318369e490ee9b770def4724d72f3f1201f5
Bhavana Nangare <60853406+akshunangare15@users.noreply.github.com>
2023-11-07 15:49:25 -0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { RepositoryListComponent } from './components/repository-list/repository-list.component';
+
+// import { UserInputComponent } from './components/user-input/user-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryListComponent,
+    // UserInputComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
